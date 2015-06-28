65e87ddc5b2e91d7e4aa499b97fd139f4aac9618
Szkiler <sowala@tlen.pl>
2015-06-29 01:59:19 +0200

    qq

--- org.szkiler.dp.server/src/main/java/org/szkiler/dp/server/App.java
+++ org.szkiler.dp.server/src/main/java/org/szkiler/dp/server/App.java
@@ -1,5 +1,8 @@
 package org.szkiler.dp.server;
 
+import org.openimaj.image.DisplayUtilities;
+import org.openimaj.video.gstreamer.GStreamerVideo;
+
 /**
  * Hello world!
  *
@@ -9,7 +12,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        
+        GStreamerVideo stream = new GStreamerVideo("play");
+        DisplayUtilities.display(stream.getCurrentFrame());
     }
     
 }
